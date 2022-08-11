1425afdb776c9dbb26cdd180fdc9bdca59604f50
Jhoana Espana <jhoespana@gmail.com>
2022-08-11 17:51:42 -0500

    terminado el localStorage

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,21 @@
 
 let portafolio = [];
 
-let agregar = document.getElementById("agregar");
+const agregar = document.getElementById("agregar");
 
 agregar.onclick = (e) => {
   e.preventDefault();
+  //validar ();
+
   let newPort = new Portfolio(titulo.value, descripcion.value, anio.value, cliente.value);
 
   checkCategoria(newPort);
   checkSoftware(newPort);
   portafolio.push(newPort);
   mostrarItemsDom();
+  
+  localStorage.setItem("portafolio", JSON.stringify(portafolio));
+
   document.getElementById("form").reset();
 };
 
@@ -34,15 +39,27 @@ function checkSoftware(newPort){
   }
 }
 
+// Validar que el formulario no este vacio
+/* function validar (){
+  const titulo = document.getElementById("titulo").value;
+  const descripcion = document.getElementById("descripcion").value;
+  
+  if(titulo.length == 0 || /^\s+$/.test(titulo)){
+    console.log("el form tuene error");
+  }
+} */
+
 function mostrarItemsDom(){
   let div = document.getElementById("portfolioContent-el");
   div.innerHTML = '';
   
   for(const item of portafolio){
+
     let contenedor = document.createElement("div");
     contenedor.classList.add("portfolioContent");
     contenedor.innerHTML += `
                             <img src="img/slide1.jpg"/>
+                            <img class="btn-trash" src="img/trash.png"/>
                             <h3 class="portfolioContent_titulo">${item.titulo}</h3>
                             <p class="portfolioContent_descripcion">${item.descripcion}</p>
                             <p class="portfolioContent_anio">${item.anio}</p>
@@ -53,3 +70,9 @@ function mostrarItemsDom(){
     div.appendChild(contenedor);
   }
 }
+
+portafolio = JSON.parse(localStorage.getItem("portafolio"));
+if(portafolio === null){
+  portafolio = [];
+} 
+mostrarItemsDom();
